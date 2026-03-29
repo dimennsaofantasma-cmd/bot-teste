022e89bc9b292ee6271e9b29ea81267bc5795a82
dimennsaofantasma-cmd <dimennsaofantasma@gmail.com>
2026-03-29 01:30:20 -0300

    Atualizar o index.js

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const { Telegraf } = require('telegraf');
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
+// Comando /start
 bot.start((ctx) => ctx.reply('Bot teste funcionando'));
+
+// Responde qualquer mensagem
+bot.on('text', (ctx) => {
+  ctx.reply('Recebi sua mensagem 👁️');
+});
 
 bot.launch();
